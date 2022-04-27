fdf20285fec6a3c653786e3e880b8aec0cc1f393
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2022-04-27 22:05:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ from discord.ext.commands import Bot
 TOKEN ='edit your token'
 intents = discord.Intents.default()
 intents.message_content = True
-
-# 메시지 앞에 접두사(prefix)가 있으면 bot.command() 이벤트가 호출됩니다.
 bot = Bot(command_prefix = '!', intents = intents)
