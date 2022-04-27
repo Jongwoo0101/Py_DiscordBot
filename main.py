617744071674733ec5a52216e822739294c7fd7e
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2022-04-27 22:03:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import discord
 from discord.ext.commands import Bot
 
-from commands.eunga_boy import EungaBoy
-from commands.game_baseball import BaseballGame
-from commands.game_gamble import GambleGame
-from commands.music import Music
-from commands.school import School
-from commands.utils import Utils
-from error_handler import ErrorHandler
-
 TOKEN ='edit your token'
 intents = discord.Intents.default()
 intents.message_content = True
